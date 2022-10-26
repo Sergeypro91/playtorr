9599d8aab304671542ae7afafaef19a4d72248b5
SergeyShevtsov <sergeyprostoas@gmail.com>
2022-10-26 20:27:53 +0300

    test: Test pre-commit

--- backend/apps/user/src/user.module.ts
+++ backend/apps/user/src/user.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './models';
 import { UserRepository } from './repositories';
 import { UserController } from './user.controller';
-import { UserService, tet343 } from './user.service';
+import { UserService, tet1 } from './user.service';
 
 @Module({
 	controllers: [UserController],
